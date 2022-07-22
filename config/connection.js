10ea4bfc6aa2173706bc3cfbebaf9c870d6dea2e
Zach Fulmer <Zachfulmer@earthlink.net>
2022-07-21 19:12:51 -0500

    corrected Sequelize call for object creation

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,7 @@ let sequelize;
 // create connection to our database, pass in your MySQL information for username and password
 if( process.env.JAWSDB_URL)
 {
-  sequelize = new sequelize(process.env.JAWSDB_URL);
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 }
 else
 {
