c33447834ae78cdf5a6e7d14e4edabf59626095b
Zach Fulmer <Zachfulmer@earthlink.net>
2022-07-21 19:20:22 -0500

    refactor connection.js file

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ if( process.env.JAWSDB_URL)
 }
 else
 {
-  const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
